09e3c3a40552d68d96a6ac681df0c92e2bf75976
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-06 23:40:56 +0900

    update

--- client.cpp
+++ client.cpp
@@ -3,50 +3,46 @@
 #include <iostream>
 #include <string>
 
-namespace asio = boost::asio;
-using asio::ip::tcp;
-
 class Client {
-    boost::asio::io_service& io_service;
-    tcp::socket socket;
+    boost::asio::io_context& io_context;
+    boost::asio::ip::tcp::socket socket;
 
    public:
-    Client(boost::asio::io_service& io_service_) : io_service(io_service_), socket(io_service_) {}
+    Client(boost::asio::io_context& io_context_) : io_context(io_context_), socket(io_context_) {}
 
-    void connect(const std::string& ip, unsigned short port) {
-        socket.async_connect(tcp::endpoint(boost::asio::ip::address::from_string(ip), port),
-                             boost::bind(&Client::on_connect, this, boost::asio::placeholders::error));
+    void connect(const boost::asio::ip::tcp::endpoint& endpoint) {
+        socket.async_connect(endpoint, [this](const boost::system::error_code& error_code) {
+            if (error_code) {
+                std::cout << "connect failed: " << error_code.message() << std::endl;
+            } else {
+                std::cout << "connect succeeded: " << socket.remote_endpoint() << std::endl;
+            }
+        });
     }
 
     void send(const std::string& data) {
-        boost::asio::async_write(socket, boost::asio::buffer(data),
-                                 boost::bind(&Client::on_send, this, boost::asio::placeholders::error, asio::placeholders::bytes_transferred));
-    }
-
-   private:
-    void on_connect(const boost::system::error_code& error_code) {
-        if (error_code) {
-            std::cout << "connect failed: " << error_code.message() << std::endl;
-            return;
-        }
-
-        send("fuck");
-    }
-
-    void on_send(const boost::system::error_code& error_code, size_t bytes_transferred) {
-        if (error_code) {
-            std::cout << "send failed: " << error_code.message() << std::endl;
-            return;
-        }
+        boost::asio::async_write(socket, boost::asio::buffer(data), [&](const boost::system::error_code& error_code, size_t bytes_transferred) {
+            if (error_code) {
+                std::cout << "send failed: " << error_code.message() << std::endl;
+            } else {
+                std::cout << "send succeeded: " << data << std::endl;
+            }
+        });
     }
 };
 
-int main() {
-    asio::io_service io_service;
+int main(int argc, char* argv[]) {
+    boost::asio::io_context io_context;
 
-    Client client(io_service);
+    Client client(io_context);
 
-    client.connect("192.168.6.116", 31400);
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(argv[1]), std::atoi(argv[2]));
+    client.connect(endpoint);
 
-    io_service.run();
+    client.send("fuck\n");
+
+    io_context.run();
+
+    while (true) {
+    }
 }
