31450d07e99c6a527019adebc46a2c91401d1397
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-08 01:34:34 +0900

    update

--- client.cpp
+++ client.cpp
@@ -1,17 +1,21 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/regex.hpp>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 class Client {
+   private:
     boost::asio::io_context& io_context;
     boost::asio::ip::tcp::socket socket;
+    std::string buffer;
 
    public:
     Client(boost::asio::io_context& io_context_) : io_context(io_context_), socket(io_context_) {}
 
     void connect(const boost::asio::ip::tcp::endpoint& endpoint) {
-        socket.async_connect(endpoint, [this](const boost::system::error_code& error_code) {
+        socket.async_connect(endpoint, [this](auto error_code) {
             if (error_code) {
                 std::cout << "connect failed: " << error_code.message() << std::endl;
             } else {
@@ -20,12 +24,26 @@ class Client {
         });
     }
 
-    void send(const std::string& data) {
-        boost::asio::async_write(socket, boost::asio::buffer(data), [&](const boost::system::error_code& error_code, size_t bytes_transferred) {
+    void send(const std::string& string) {
+        boost::asio::async_write(socket, boost::asio::buffer(string), [this, string](auto error_code, auto size) {
             if (error_code) {
                 std::cout << "send failed: " << error_code.message() << std::endl;
             } else {
-                std::cout << "send succeeded: " << data << std::endl;
+                std::cout << "send succeeded: " << string << std::endl;
+            }
+        });
+    }
+
+    void receive() {
+        boost::asio::async_read_until(socket, boost::asio::dynamic_buffer(buffer), boost::regex("<s>.*</s>"), [this](auto error_code, auto size) {
+            if (error_code) {
+                std::cout << "receive failed: " << error_code.message() << std::endl;
+
+                socket.close();
+
+            } else {
+                std::string string = buffer.substr(3, size - 7);
+                std::cout << "receive succeeded: " << string << std::endl;
             }
         });
     }
@@ -38,8 +56,13 @@ int main(int argc, char* argv[]) {
 
     boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(argv[1]), std::atoi(argv[2]));
     client.connect(endpoint);
-
-    client.send("fuck\n");
-
     io_context.run();
+
+    std::string string;
+    while (std::getline(std::cin, string)) {
+        io_context.restart();
+        client.send("<s>" + string + "</s>");
+        client.receive();
+        io_context.run();
+    }
 }
