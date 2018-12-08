571e67503af905bbe69e3e681f1032d09e9a27a0
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-12-08 12:06:28 +0900

    update

--- client.cpp
+++ client.cpp
@@ -11,7 +11,10 @@ class Client {
    private:
     boost::asio::io_context& io_context;
     boost::asio::ip::tcp::socket socket;
-    std::string buffer;
+
+   public:
+    std::string head;
+    std::string tail;
 
    public:
     Client(boost::asio::io_context& io_context_) : io_context(io_context_), socket(io_context_) {}
@@ -28,7 +31,7 @@ class Client {
     }
 
     void send(const std::string& string) {
-        boost::asio::async_write(socket, boost::asio::buffer(string), [this, string](auto error_code, auto size) {
+        boost::asio::async_write(socket, boost::asio::buffer("<s>" + string + "</s>"), [this, string](auto error_code, auto size) {
             if (error_code) {
                 std::cout << "send failed: " << error_code.message() << std::endl;
             } else {
@@ -40,7 +43,7 @@ class Client {
     }
 
     void receive() {
-        boost::asio::async_read_until(socket, boost::asio::dynamic_buffer(buffer), boost::regex("<s>.*</s>"), [this](auto error_code, auto size) {
+        boost::asio::async_read_until(socket, boost::asio::dynamic_buffer(tail), boost::regex("<s>.*</s>"), [this](auto error_code, auto size) {
             if (error_code) {
                 std::cout << "receive failed: " << error_code.message() << std::endl;
 
@@ -50,29 +53,27 @@ class Client {
                 std::string start_token = "<s>";
                 std::string end_token = "</s>";
 
-                std::string string(std::search(buffer.begin(), buffer.end(), start_token.begin(), start_token.end()) + start_token.size(),
-                                   std::search(buffer.begin(), buffer.end(), end_token.begin(), end_token.end()));
-                std::cout << "receive succeeded: " << string << std::endl;
+                head = std::string(std::search(tail.begin(), tail.end(), start_token.begin(), start_token.end()) + start_token.size(),
+                                   std::search(tail.begin(), tail.end(), end_token.begin(), end_token.end()));
+                std::cout << "receive succeeded: " << head << std::endl;
 
-                buffer.erase(size);
+                tail.erase(0, size);
             }
         });
     }
-
-    const std::string& get_buffer() const { return buffer; }
 };
 
 PYBIND11_MODULE(client, module) {
-    module.def("request", [](const std::string& ip, unsigned short port, const std::string& string) {
+    module.def("request", [](const std::string& ip_address, const std::string& port_number, const std::string& string) {
         boost::asio::io_context io_context;
         Client client(io_context);
 
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
+        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip_address), std::stoi(port_number));
         client.connect(endpoint);
-        client.send("<s>" + string + "</s>");
+        client.send(string);
 
         io_context.run();
 
-        return client.get_buffer();
+        return client.head;
     });
 }
--- client_app.py
+++ client_app.py
@@ -1,5 +1,9 @@
+import numpy as np
+import cv2
 import sys
+import base64
 import client
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
@@ -11,6 +15,7 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.view_layout = QVBoxLayout()
+
         self.view = QGraphicsView()
         self.scene = QGraphicsScene()
         self.scene.setSceneRect(0, 0, 512, 512)
@@ -18,6 +23,24 @@ class MainWindow(QWidget):
         self.scene.addItem(self.pixmap)
         self.view.setScene(self.scene)
         self.view_layout.addWidget(self.view)
+
+        self.property_layout = QVBoxLayout()
+
+        self.textbox_layout_1 = QGridLayout()
+
+        self.ip_address_label = QLabel("IP Address")
+        self.textbox_layout_1.addWidget(self.ip_address_label, 0, 0)
+
+        self.ip_address_textbox = QLineEdit("192.168.1.4")
+        self.textbox_layout_1.addWidget(self.ip_address_textbox, 0, 1)
+
+        self.port_number_label = QLabel("Port Number")
+        self.textbox_layout_1.addWidget(self.port_number_label, 1, 0)
+
+        self.port_number_textbox = QLineEdit("1234")
+        self.textbox_layout_1.addWidget(self.port_number_textbox, 1, 1)
+
+        self.property_layout.addLayout(self.textbox_layout_1)
 
         self.button_layout = QVBoxLayout()
 
@@ -29,9 +52,20 @@ class MainWindow(QWidget):
         self.request_button.clicked.connect(self.request)
         self.button_layout.addWidget(self.request_button)
 
+        self.property_layout.addLayout(self.button_layout)
+
+        self.textbox_layout_2 = QVBoxLayout()
+
+        self.annotation_textboxes = [QLineEdit() for _ in range(4)]
+        for annotation_textbox in self.annotation_textboxes:
+            self.textbox_layout_2.addWidget(annotation_textbox)
+
+        self.property_layout.addLayout(self.textbox_layout_2)
+
         self.main_layout = QHBoxLayout()
+
         self.main_layout.addLayout(self.view_layout)
-        self.main_layout.addLayout(self.button_layout)
+        self.main_layout.addLayout(self.property_layout)
 
         self.setLayout(self.main_layout)
         self.setWindowTitle("Attention OCR")
@@ -39,14 +73,24 @@ class MainWindow(QWidget):
     def open(self):
 
         filename, _ = QFileDialog.getOpenFileName(self, "Open file", ".")
-        self.pixmap.setPixmap(QPixmap(filename).scaled(self.scene.sceneRect().size().toSize()))
+        self.image = QImage(filename)
+        self.pixmap.setPixmap(QPixmap.fromImage(self.image).scaled(self.scene.sceneRect().size().toSize()))
 
     def request(self):
 
-        client.request("127.0.0.1", 1234, "image.jpg")
+        image = self.image.scaled(256, 256)
+        byte_array = QByteArray()
+        buffer = QBuffer(byte_array)
+        buffer.open(QIODevice.WriteOnly)
+        image.save(buffer, "PNG")
+        base64_encoded = byte_array.toBase64().data()
+
+        annotations = client.request(self.ip_address_textbox.text(), self.port_number_textbox.text(), base64_encoded)
+        for annotation_textbox, annotation in zip(self.annotation_textboxes, annotations.split("_")):
+            annotation_textbox.setText(annotation)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
